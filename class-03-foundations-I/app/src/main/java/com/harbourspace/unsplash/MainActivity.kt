f8477d45276ad2d6b83c45ed6a414852f1dc15be
Carlos Mota <478962+cmota@users.noreply.github.com>
2026-06-11 08:45:16 +0200

    Class #03 Materials

---
+++ class-03-foundations-I/app/src/main/java/com/harbourspace/unsplash/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.harbourspace.unsplash
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.harbourspace.unsplash.ui.theme.UnsplashTheme
+
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+private const val TAG = "MainActivity"
+
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+
+    Log.d(TAG, "onCreate")
+
+    enableEdgeToEdge()
+    setContent {
+      UnsplashTheme {
+        Scaffold(
+          modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+          Column(
+            modifier = Modifier
+              .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+          ) {
+            var count by rememberSaveable { mutableIntStateOf(0) }
+
+            Greeting(
+              name = "World (${count}) 🌍",
+              modifier = Modifier
+                .background(Color.Red)
+            )
+
+            Image(
+              painter = painterResource(R.drawable.ic_launcher_foreground),
+              contentDescription = stringResource(R.string.greeting)
+            )
+
+            TextButton(onClick = {
+              count += 1
+            } ) {
+              Text("+")
+            }
+          }
+        }
+      }
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+
+    Log.d(TAG, "onResume")
+  }
+
+  override fun onPause() {
+    Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+
+    Log.d(TAG, "onPause")
+
+    super.onPause()
+  }
+
+  override fun onDestroy() {
+    Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+
+    Log.d(TAG, "onDestroy")
+
+    super.onDestroy()
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = stringResource(id = R.string.greeting, name),
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  UnsplashTheme {
+    Greeting("World 🌍")
+  }
+}
